a358da5ac89d962482f6723443cb55f0893b31bd
Caleb Kress <calebkress@gmail.com>
2019-12-20 17:35:37 -0600

    grabs list of all data, needs filtering into dict

--- request.py
+++ request.py
@@ -1,5 +1,18 @@
-import requests
+import praw
 
-response = requests.get('http://www.reddit.com/r/all/top/')
+reddit = praw.Reddit(client_id='id',
+                     client_secret='secret',
+                     password='password',
+                     user_agent='/all/top grabber script by /u/cbkatx',
+                     username='cbkatx')
 
-print(response)
+top_posts = []
+
+for submission in reddit.subreddit('all').top('hour', limit = 5):
+    top_posts.append(submission.author)
+    top_posts.append(submission.comments)
+    top_posts.append(submission.subreddit)
+    top_posts.append(submission.title)
+    
+
+print(top_posts)
